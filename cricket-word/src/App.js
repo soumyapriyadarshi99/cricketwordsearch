826d624f761e90b5b02f7ad0e37f80993d93c8cc
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2022-11-05 19:10:26 +0530

    work done

--- cricket-word/src/App.js
+++ cricket-word/src/App.js
@@ -1,63 +1,43 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React from "react";
 import { useState } from "react";
 import cricketWords from "./utils/cricketwords";
 import matrix from "./utils/matrix";
+import { row } from "./utils/matrix";
+import { col } from "./utils/matrix";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import wordCalculator from "./components/wordCalculator";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  // const [searchValue, setSearchValue] = useState([]);
   const [cricketWord, setCricketWord] = useState(cricketWords);
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
-  useEffect(() => {
-    wordFinder();
-  },[]);
 
   const wordFinder = () => {
-    // for (let i = 0; i < searchValue.length; i++) {
-    //   for (let j = 0; j < cricketWords.length; j++) {
-    //     if (
-    //       searchValue[i].toUpperCase() ===
-    //       cricketWords[j].name.toUpperCase()
-    //     ) {
-
-    //       cricketWords[j].displayColor = true;
-    //       console.log(cricketWords[j]);
-    //       // wordFinder();
-    //     }
-    //   }
-    // }
-    console.log(cricketWord.length)
     for (let i = 0; i < cricketWord.length; i++) {
       if (inputValue.toUpperCase() === cricketWord[i].name.toUpperCase()) {
         cricketWord[i].displayColor = true;
-        cricketWord.pop(cricketWord[i]);
-        setCricketWord([...cricketWord,cricketWord[i]]);
-        
-        // console.log(cricketWord[i]);
-        // console.log(cricketWord);
+        setCricketWord([...cricketWord, cricketWord[i]]);
         break;
-      }
-      // else {
-      //   console.log(cricketWord)
-      // }
+      } 
     }
   };
 
   const onClickHandler = () => {
-    console.log('onclick ')
-  wordFinder();
+    wordFinder();
   };
+  console.log(wordCalculator())
 
   return (
     <div className="App">
       <h1 className="container-heading">Cricket Word Search</h1>
       <div className="word-matrix-container">
         <div className="word-container">
-          <h3 style={{ textAllign: "center" }}>Find The Words</h3>
+          <h3 style={{ textAlign: "center" }}>Find The Words</h3>
           <ul className="cricketWordlist">
             {cricketWords.map((item, index) => {
               return (
@@ -75,9 +55,10 @@ function App() {
               name="search-words"
               id="search-words"
               placeholder="search words"
+              className="input"
               onChange={handleChange}
             />
-            <button type="submit" onClick={onClickHandler}>
+            <button className="button" type="submit" onClick={onClickHandler}>
               Submit
             </button>
           </div>
---
+++ cricket-word/src/components/wordCalculator.js
@@ -0,0 +1,11 @@
+import { row } from "../utils/matrix";
+import { col } from "../utils/matrix";
+import matrix from "../utils/matrix";
+const wordCalculator = (inputValue='hello') => {
+    console.log(row);
+    console.log(col);
+    const height = matrix.length;
+    const width = matrix[0].length;
+    
+}
+export default wordCalculator;
